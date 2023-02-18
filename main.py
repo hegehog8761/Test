d91eef0f1e0b9422c49bc20d19f322c838ff5799
hegehog8761 <119626198+hegehog8761@users.noreply.github.com>
2023-02-18 13:53:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import ctypes
 
-ctypes.windll.user32.MessageBoxW(0, u"It worked", u"Lol, it worked", 0)
+ctypes.windll.user32.MessageBoxW(0, u"You fell for it", u"This file could have encrypted all of your data and you didn't even notice it was a trojen.", 0)
